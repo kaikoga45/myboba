c1e81a4a4653398ab83d4922e89ce35d0eb1c7d7
ArsanaPutra <arsanaputra008@gmail.com>
2020-12-24 23:02:20 +0800

    add code on welcome_screen.dart

--- lib/ui/screens/welcome_screens.dart
+++ lib/ui/screens/welcome_screens.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myboba/ui/screens/welcome_screens.dart';
+import 'package:myboba/ui/theme/main_theme.dart';
 
 class WelcomeScreen extends StatelessWidget {
   static const String id = '/welcome_screen';
@@ -7,7 +9,29 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                left: 100,
+                right: 100,
+              ),
+              child: Text("Save time ordering beverage with MyBoba",
+                style: Theme.of(context).textTheme.headline5.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+
+          ],
+        ),
       ),
     );
   }

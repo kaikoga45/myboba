237ce4096c4ababcb3c03284d58dca0ec0d995ff
Kai Koga <koga.kai@icloud.com>
2020-12-24 15:06:20 +0800

    Changing Firebase Init Proccess and Adding Error Alert Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,50 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myboba/ui/components/error_alert.dart';
 import 'package:myboba/ui/screens/homepage_screens.dart';
 import 'package:myboba/ui/screens/welcome_screens.dart';
 import 'package:myboba/ui/theme/main_theme.dart';
 
-bool isLogin = true;
-var initialRoute;
+bool _isLogin = true;
+var _initialRoute;
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  checkUserLogin();
-  runApp(MyBoba());
+  runApp(InitApp());
+}
+
+class InitApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: themeData.primaryColor,
+      child: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return ErrorAlert();
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyBoba();
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
 }
 
 class MyBoba extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    checkUserLogin();
     return MaterialApp(
       title: 'MyBoba',
       theme: themeData,
-      initialRoute: initialRoute,
+      initialRoute: _initialRoute,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
         HomepageScreens.id: (context) => HomepageScreens(),
@@ -31,9 +55,9 @@ class MyBoba extends StatelessWidget {
 
 void checkUserLogin() {
   // TODO : Adding firebase auth function to check user login
-  if (!isLogin) {
-    initialRoute = WelcomeScreen.id;
+  if (!_isLogin) {
+    _initialRoute = WelcomeScreen.id;
   } else {
-    initialRoute = HomepageScreens.id;
+    _initialRoute = HomepageScreens.id;
   }
 }

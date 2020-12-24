ae67450cf28b2b239a6beb9beabcd9bdfe0b5453
Kai Koga <koga.kai@icloud.com>
2020-12-24 21:58:19 +0800

    Rename "header_footer" to "footer"

---
+++ lib/ui/components/footer.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:myboba/utils/list_widgets.dart';
+
+class Footer extends StatefulWidget {
+  static const id = '/footer';
+
+  @override
+  _FooterState createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetOptionsAtHomepage.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Theme.of(context).iconTheme.color,
+        unselectedItemColor: Color(0xFFB8B7B4),
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.coffee), label: 'Menu'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.receipt), label: 'Receipt'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+        onTap: (values) {
+          setState(() {
+            _selectedIndex = values;
+          });
+        },
+      ),
+    );
+  }
+}

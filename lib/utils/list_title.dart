988767ffadddd184f7b84f4eb0b4d12e6470481f
Kai Koga <koga.kai@icloud.com>
2020-12-25 23:36:19 +0800

    Adding a file to store all list contain the title

---
+++ lib/utils/list_title.dart
@@ -0,0 +1,5 @@
+final Map<String, String> shortMessageEachCategory = {
+  'featured': 'Boost up your work routine with our featured beverage',
+  'popular': 'Try some our beverage that people love',
+  'recommendation': 'Here our recommendation beverage just for you',
+};

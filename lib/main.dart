af83d20135d909aa810af9a0778599abcb1fe1a3
Kai Koga <koga.kai@icloud.com>
2020-12-26 12:14:42 +0800

    Renaming the old new dependancy path to the new one

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myboba/ui/components/error_alert.dart';
 import 'package:myboba/ui/components/footer.dart';
+import 'package:myboba/ui/screens/display_all_menu.dart';
 import 'package:myboba/ui/screens/homepage.dart';
 import 'package:myboba/ui/screens/menu.dart';
 import 'package:myboba/ui/screens/receipt.dart';
-import 'package:myboba/ui/screens/see_all_menu.dart';
 import 'package:myboba/ui/screens/settings.dart';
 import 'package:myboba/ui/screens/welcome_screens.dart';
 import 'package:myboba/ui/theme/main_theme.dart';
@@ -57,7 +57,7 @@ class MyBoba extends StatelessWidget {
         Menu.id: (context) => Menu(),
         Receipt.id: (context) => Receipt(),
         Settings.id: (context) => Settings(),
-        SeeAllMenu.id: (context) => SeeAllMenu(),
+        DisplayAllMenu.id: (context) => DisplayAllMenu(),
       },
     );
   }

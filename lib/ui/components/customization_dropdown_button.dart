31ed1ac6bf46fea982ad919a9bf5293ab1533e50
Kai Koga <koga.kai@icloud.com>
2020-12-29 19:08:39 +0800

    Creating a custom dropdown button for order screen

---
+++ lib/ui/components/customization_dropdown_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomizationDropdownButton extends StatelessWidget {
+  final String _title;
+  final String _value;
+  final List<DropdownMenuItem<Object>> _items;
+  final Function _onChanged;
+
+  CustomizationDropdownButton(
+      {@required String title,
+      @required String value,
+      @required List<DropdownMenuItem<Object>> items,
+      @required Function onChanged})
+      : _title = title,
+        _value = value,
+        _items = items,
+        _onChanged = onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              _title,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Spacer(),
+            DropdownButtonHideUnderline(
+              child: DropdownButton(
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle1
+                      .copyWith(color: Theme.of(context).buttonColor),
+                  isDense: true,
+                  elevation: 0,
+                  dropdownColor: Color(0xFFEDE2CF),
+                  icon: Icon(Icons.arrow_forward_ios,
+                      color: Theme.of(context).buttonColor),
+                  iconSize: 15,
+                  value: _value,
+                  items: _items,
+                  onChanged: _onChanged),
+            ),
+          ],
+        ),
+        Divider(color: Color(0xFFC99543)),
+      ],
+    );
+  }
+}

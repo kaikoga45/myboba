4c611e9d4a271b21012633787731089f4fd96ba1
Kai Koga <koga.kai@icloud.com>
2020-12-30 01:02:18 +0800

    Adding a CRUD for category data from firestore

---
+++ lib/ui/staff/screens/category_page/read_category.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:myboba/services/firebase/staff_firestore_helper.dart';
+import 'package:myboba/ui/staff/screens/category_page/create_update_category.dart';
+
+class ReadCategory extends StatelessWidget {
+  final _firestore = StaffFirestoreHelper.firestore;
+  final _staffFirestoreHelper = StaffFirestoreHelper.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'CATEGORY',
+          style: Theme.of(context).textTheme.headline6.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Container(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: _firestore.collection('category').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot _category = snapshot.data.docs[index];
+                    return Card(
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  CreateUpdateCategory(categoryDoc: _category),
+                            ),
+                          );
+                        },
+                        contentPadding: EdgeInsets.symmetric(vertical: 10),
+                        leading: CircleAvatar(
+                          radius: 50.0,
+                          child: FittedBox(
+                            child: Text(
+                              '${index + 1}',
+                            ),
+                          ),
+                        ),
+                        title: Text(_category['name']),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            bool _isError;
+                            _isError =
+                                await _staffFirestoreHelper.deleteCategory(
+                                    docId: _category.id,
+                                    category: _category['name']);
+                            _isError
+                                ? Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text('Failed to delete!')))
+                                : Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text('Delete completed!')));
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => CreateUpdateCategory()));
+        },
+      ),
+    );
+  }
+}

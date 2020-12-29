47ab7fe65cbc13aca10780cdabcbd98bff98a09a
Kai Koga <koga.kai@icloud.com>
2020-12-29 18:57:01 +0800

    Adding a auth helper instance and set temporary user id

--- lib/services/firebase/authentication.dart
+++ lib/services/firebase/authentication.dart
@@ -2,4 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthHelper {
   static final auth = FirebaseAuth.instance;
+
+  AuthHelper._privateConstructor();
+  static final AuthHelper instance = AuthHelper._privateConstructor();
+
+  //Temp
+  String customerId = '1';
 }

96a5a06299164fdc9b7fb55c71bc46d512e2a353
Kai Koga <koga.kai@icloud.com>
2020-12-29 18:53:00 +0800

    Adding a two tab for displaying a active order and list of transaction

--- lib/ui/screens/receipt.dart
+++ lib/ui/screens/receipt.dart
@@ -1,29 +1,52 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myboba/ui/components/stream_receipt.dart';
 
 class Receipt extends StatelessWidget {
   static const String id = '/receipt';
 
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'RECEIPT ORDER',
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(fontWeight: FontWeight.bold),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          elevation: 0,
+          bottom: TabBar(
+            indicatorColor: Theme.of(context).accentColor,
+            indicatorWeight: 5.0,
+            tabs: [
+              Tab(
+                child: Text('ACTIVE ORDER',
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2
+                        .copyWith(fontSize: 14, fontWeight: FontWeight.w400)),
+              ),
+              Tab(
+                child: Text('LIST TRANSACTION',
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2
+                        .copyWith(fontSize: 14, fontWeight: FontWeight.w400)),
+              ),
+            ],
+          ),
+          title: Text(
+            'RECEIPT ORDER',
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
         ),
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: TabBarView(
           children: [
-            Container(
-              color: Colors.orange,
-              height: 100,
+            StreamReceipt(
+              isPickup: false,
+            ),
+            StreamReceipt(
+              isPickup: true,
             ),
           ],
         ),

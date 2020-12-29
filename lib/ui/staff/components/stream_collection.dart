ff1e227a55a671474e35d8ef9229a968ab4acaa1
Kai Koga <koga.kai@icloud.com>
2020-12-30 00:54:56 +0800

    Creating a stream builder for fetching all total data in every each of collection

--- lib/ui/staff/components/stream_collection.dart
+++ lib/ui/staff/components/stream_collection.dart
@@ -7,44 +7,63 @@ class StreamCollection extends StatelessWidget {
   final String _collection;
   final String _title;
   final Color _color;
+  final bool _isOrder;
+  final Function _onTap;
 
-  StreamCollection({String collection, String title, Color color})
+  StreamCollection(
+      {String collection,
+      String title,
+      Color color,
+      bool isOrder,
+      Function onTap})
       : _collection = collection,
         _title = title,
-        _color = color;
+        _color = color,
+        _isOrder = isOrder,
+        _onTap = onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: _color,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            _title.toUpperCase(),
-            style: Theme.of(context)
-                .textTheme
-                .headline4
-                .copyWith(color: Theme.of(context).primaryColor),
-          ),
-          StreamBuilder<QuerySnapshot>(
-            stream:
-                _firestore.collection(_collection.toLowerCase()).snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return Text(
-                  '${snapshot.data.docs.length} Total',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText1
-                      .copyWith(color: Colors.black38),
-                );
-              }
-            },
-          ),
-        ],
+    return GestureDetector(
+      onTap: _onTap,
+      child: Card(
+        color: _color,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _title.toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(color: Theme.of(context).primaryColor),
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: _isOrder
+                  ? _firestore
+                      .collection('order')
+                      .where('pickup', isEqualTo: true)
+                      .where('checkout', isEqualTo: true)
+                      .snapshots()
+                  : _firestore
+                      .collection(_collection.toLowerCase())
+                      .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  return Text(
+                    '${snapshot.data.docs.length} Total',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        .copyWith(color: Colors.black38),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

601da19622243b8852004a0c33bb9ed878b81d47
Kai Koga <koga.kai@icloud.com>
2020-12-30 00:53:36 +0800

    Changing the widget name

--- lib/utils/customer/list_of_widgets.dart
+++ lib/utils/customer/list_of_widgets.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:myboba/ui/customer/screens/homepage.dart';
+import 'package:myboba/ui/customer/screens/customer_home_page.dart';
 import 'package:myboba/ui/customer/screens/menu.dart';
 import 'package:myboba/ui/customer/screens/receipt.dart';
 import 'package:myboba/ui/customer/screens/settings.dart';
 
 final List<Widget> widgetOptionsAtHomepage = [
-  HomePage(),
+  CustomerHomePage(),
   Menu(),
   Receipt(),
   Settings(),

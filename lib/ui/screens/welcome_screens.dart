6f5dd41e12d43ff613dc9eb76eaf0309778d478c
Kai Koga <koga.kai@icloud.com>
2021-01-02 14:28:36 +0800

    Changing to use expanded

--- lib/ui/screens/welcome_screens.dart
+++ lib/ui/screens/welcome_screens.dart
@@ -7,21 +7,27 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double widthScreen = MediaQuery.of(context).size.width;
-    double heightScreen = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(height: (heightScreen * 0.14)),
-            _heading(context),
-            SizedBox(height: heightScreen * 0.078),
-            _picture(),
-            SizedBox(height: heightScreen * 0.202),
-            _buttonSignUp(context),
-            _buttonSignIn(context),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          child: Column(
+            children: [
+              Expanded(child: _heading(context)),
+              Expanded(child: _picture()),
+              Expanded(
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      _buttonSignUp(context),
+                      _buttonSignIn(context),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -29,16 +35,19 @@ class WelcomeScreen extends StatelessWidget {
 
   Widget _heading(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(
-        left: 100,
-        right: 100,
-      ),
-      child: Text(
-        "Save time ordering beverage with MyBoba",
-        style: Theme.of(context).textTheme.headline5.copyWith(
-              fontWeight: FontWeight.w900,
-            ),
-        textAlign: TextAlign.center,
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Save time ordering beverage with MyBoba",
+            style: Theme.of(context).textTheme.headline5.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+            textAlign: TextAlign.center,
+            maxLines: 3,
+          ),
+        ],
       ),
     );
   }
@@ -60,42 +69,40 @@ class WelcomeScreen extends StatelessWidget {
   Widget _buttonSignUp(BuildContext context) {
     double widthScreen = MediaQuery.of(context).size.width;
 
-    return 
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 23),
-          child: FlatButton(
-            child: Text("SIGN UP"),
-            onPressed: () {
-              Navigator.pushNamed(context, "/signup");
-            },
-            textColor: Colors.white,
-            color: Color(0xFFC99542),
-            padding: EdgeInsets.all(20),
-            minWidth: widthScreen,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100)),
-          ),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 23),
+      child: FlatButton(
+        child: Text("SIGN UP"),
+        onPressed: () {
+          Navigator.pushNamed(context, "/signup");
+        },
+        textColor: Colors.white,
+        color: Color(0xFFC99542),
+        padding: EdgeInsets.all(20),
+        minWidth: widthScreen,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+      ),
     );
   }
 
-  Widget _buttonSignIn(BuildContext context){
+  Widget _buttonSignIn(BuildContext context) {
     return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Already have an account? ",
-              style: Theme.of(context).textTheme.subtitle2,
-            ),
-            Container(
-              child: InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, "/signin");
-                },
-                child: Text("Sign In"),
-              ),
-            ),
-          ],
-        );
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Already have an account? ",
+          style: Theme.of(context).textTheme.subtitle2,
+        ),
+        Container(
+          child: InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, "/signin");
+            },
+            child: Text("Sign In"),
+          ),
+        ),
+      ],
+    );
   }
 }
